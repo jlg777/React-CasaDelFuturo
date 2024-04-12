0a8268b49aabf52cef0c97b60b7825a991a1dda4
= <=>
2024-04-12 12:23:19 -0300

    update

---
+++ src/assets/taskList.js
@@ -0,0 +1,30 @@
+// miArray.js
+const miArray = [
+  {
+    id: 1,
+    tarea: "repasar contenido js",
+    cumplido: true,
+  },
+  {
+    id: 2,
+    tarea: "repasar react",
+    cumplido: false,
+  },
+  {
+    id: 3,
+    tarea: "repasar teoria",
+    cumplido: false,
+  },
+  {
+    id: 4,
+    tarea: "realizar tp",
+    cumplido: false,
+  },
+  {
+    id: 5,
+    tarea: "subir a github",
+    cumplido: false,
+  },
+];
+
+export default miArray;
--- src/complements/TaskList.jsx
+++ src/complements/TaskList.jsx
@@ -1,22 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import taskList from "../assets/taskList";
+import "../assets/css/tasklist.css";
 
 const TaskList = () => {
+  const [tasks, setTasks] = useState(taskList);
+  const [newTask, setNewTask] = useState("");
+
+  const handleTask = (e) => {
+    setNewTask(e.target.value);
+    console.log(newTask);
+  };
+
+  const handleButton = () => {
+    setTasks([
+      ...tasks,
+      { id: tasks.length + 1, tarea: newTask, cumplido: false },
+    ]);
+  };
+
+  const handleCumplido = (taskId) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === taskId) {
+          return { ...task, cumplido: !task.cumplido };
+        }
+        return task;
+      })
+    );
+  };
+
   return (
     <>
+      <h1> Lista de Tareas</h1>
       <fieldset>
         <ul>
-          Lista de Tareas
+          {/*}
           <li>
             Modelo de tarea 1 ❌ <button>🗑️</button>
           </li>
           <li>
             Modelo de tarea 2 ✔️ <button>🗑️</button>
           </li>
+  {*/}
+          {tasks.map((task) => (
+            <li key={task.id}>
+              {task.tarea}
+              <div>
+                <button onClick={() => handleCumplido(task.id)}>
+                  {task.cumplido ? "✔️" : "❌"}{" "}
+                </button>
+                <button>🗑️</button>
+              </div>
+            </li>
+          ))}
         </ul>
       </fieldset>
-
-      <input placeholder="Ingrese una tarea" />
-      <button>Cargar</button>
+      <input
+        placeholder="Ingrese una tarea"
+        onChange={handleTask}
+        value={newTask}
+      />
+      <button onClick={handleButton}>Cargar</button>
     </>
   );
 };
