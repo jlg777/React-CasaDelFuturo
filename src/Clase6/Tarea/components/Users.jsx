bb3eea1c6a6c1a066e4034165461f14d8a23cd69
= <=>
2024-05-08 17:28:53 -0300

    update

---
+++ src/Clase6/Tarea/components/Users.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+const Users = () => {
+  const [respuestaAPI, setRespuestaAPI] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://randomuser.me/api/?results=10");
+      const data = await response.json();
+      setRespuestaAPI(data.results);
+      //console.log(data);
+    } catch (error) {
+      console.error("Error al obtener datos:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {respuestaAPI.map((user) => (
+        <div className="card" style={{ width: "18rem" }} key={user.login.uuid}>
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{user.login.uuid}</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+export default Users;
--- src/main.jsx
+++ src/main.jsx
@@ -5,25 +5,6 @@ import "./index.css";
 import { FilterProviders } from "./Clase5-Carrito/providers/filterProviders";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Rutas from "./Clase6/complements/Rutas";
+import Users from "./Clase6/Tarea/components/Users";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-<BrowserRouter>
-<Routes>
-  <Route path="index">
-    <Route index element={<h1>index MAIN</h1>}/>
-    <Route path="main" element={<h1>index TWO</h1>}/>
-  </Route>
-  <Route path="about" >
-  <Route index element={<h1>about</h1>}/>
-    <Route path="main" element={<h1>about TWO</h1>}/>
-  </Route> 
-  <Route path="home" element={<Rutas />}>
-    <Route index element={<h1>home</h1>}/>
-    <Route path="index" element={<h1>index</h1>}/>
-    <Route path="about" element={<h1>about</h1>}/>
-    <Route path="main" element={<h1>main</h1>}/>
-  </Route>
-  <Route path="*" element={<h1>404 - not found</h1>}/>
-</Routes>
-</BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<Users />);
